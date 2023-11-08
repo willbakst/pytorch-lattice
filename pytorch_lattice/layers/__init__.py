bb64e952c2cb92628b5007f082d39ed5c00c0af8
Brian Park <brianpark@brians-air-2.mynetworksettings.com>
2023-11-08 18:08:28 -0500

    chore: port lattice and lattice tests

--- pytorch_lattice/layers/__init__.py
+++ pytorch_lattice/layers/__init__.py
@@ -1,4 +1,5 @@
 """Layers used in calibrated modeling implemented as `torch.nn.Module`."""
 from .categorical_calibrator import CategoricalCalibrator
+from .lattice import Lattice
 from .linear import Linear
 from .numerical_calibrator import NumericalCalibrator
---
+++ pytorch_lattice/models/calibrated_lattice.py
@@ -0,0 +1,173 @@
+"""Class for easily constructing a calibrated lattice model."""
+from typing import Dict, List, Optional, Union
+
+import torch
+
+from ..enums import (
+    Monotonicity,
+    LatticeInit,
+    Interpolation,
+)
+from .features import CategoricalFeature, NumericalFeature
+from ..layers import Lattice
+from .model_utils import (
+    initialize_feature_calibrators,
+    initialize_output_calibrator,
+    initialize_monotonicities,
+    calibrate_and_stack,
+)
+
+
+# pylint: disable-next=too-many-instance-attributes
+class CalibratedLattice(torch.nn.Module):
+    """PyTorch Calibrated Lattice Model.
+
+    Creates a `torch.nn.Module` representing a calibrated lattice model, which will be
+    constructed using the provided model configuration. Note that the model inputs
+    should match the order in which they are defined in the `feature_configs`.
+
+    Attributes:
+        - All `__init__` arguments.
+        calibrators: A dictionary that maps feature names to their calibrators.
+        lattice: The `Lattice` layer of the model.
+        output_calibrator: The output `NumericalCalibrator` calibration layer. This
+            will be `None` if no output calibration is desired.
+
+    Example:
+
+    ```python
+    csv_data = CSVData(...)
+
+    feature_configs = [...]
+    calibrated_model = CalibratedLattice(feature_configs, ...)
+
+    loss_fn = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(calibrated_model.parameters(recurse=True), lr=1e-1)
+
+    csv_data.prepare(feature_configs, "target", ...)
+    for epoch in range(100):
+        for examples, targets in csv_data.batch(64):
+            optimizer.zero_grad()
+            outputs = calibrated_model(inputs)
+            loss = loss_fn(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            calibrated_model.constrain()
+    ```
+    """
+
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        features: List[Union[NumericalFeature, CategoricalFeature]],
+        clip_inputs: bool = True,
+        output_min: Optional[float] = None,
+        output_max: Optional[float] = None,
+        kernel_init: LatticeInit = LatticeInit.LINEAR,
+        interpolation: Interpolation = Interpolation.HYPERCUBE,
+        output_calibration_num_keypoints: Optional[int] = None,
+    ) -> None:
+        """Initializes an instance of `CalibratedLattice`.
+
+        Args:
+            features: A list of numerical and/or categorical feature configs.
+            clip_inputs: Whether to restrict inputs to the bounds of lattice.
+            output_min: The minimum output value for the model. If `None`, the minimum
+                output value will be unbounded.
+            output_max: The maximum output value for the model. If `None`, the maximum
+                output value will be unbounded.
+            kernel_init: the method of initializing kernel weights. If otherwise
+                unspecified, will default to `LatticeInit.LINEAR`.
+            interpolation: the method of interpolation in the lattice's forward pass.
+                If otherwise unspecified, will default to `Interpolation.HYPERCUBE`.
+            output_calibration_num_keypoints: The number of keypoints to use for the
+                output calibrator. If `None`, no output calibration will be used.
+
+        Raises:
+            ValueError: If any feature configs are not `NUMERICAL` or `CATEGORICAL`.
+        """
+        super().__init__()
+
+        self.features = features
+        self.clip_inputs = clip_inputs
+        self.output_min = output_min
+        self.output_max = output_max
+        self.kernel_init = kernel_init
+        self.interpolation = interpolation
+        self.output_calibration_num_keypoints = output_calibration_num_keypoints
+        self.monotonicities = initialize_monotonicities(features)
+        self.calibrators = initialize_feature_calibrators(
+            features=features,
+            output_min=0,
+            output_max=[feature.lattice_size - 1 for feature in features],
+        )
+
+        self.lattice = Lattice(
+            lattice_sizes=[feature.lattice_size for feature in features],
+            monotonicities=self.monotonicities,
+            clip_inputs=self.clip_inputs,
+            output_min=self.output_min,
+            output_max=self.output_max,
+            interpolation=interpolation,
+            kernel_init=kernel_init,
+        )
+
+        self.output_calibrator = initialize_output_calibrator(
+            output_calibration_num_keypoints=output_calibration_num_keypoints,
+            monotonic=not all(m == Monotonicity.NONE for m in self.monotonicities),
+            output_min=output_min,
+            output_max=output_max,
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:  # pylint: disable=invalid-name
+        """Runs an input through the network to produce a calibrated lattice output.
+
+        Args:
+            x: The input tensor of feature values of shape `(batch_size, num_features)`.
+
+        Returns:
+            torch.Tensor of shape `(batch_size, 1)` containing the model output result.
+        """
+        result = calibrate_and_stack(x, self.calibrators)
+        result = self.lattice(result)
+        if self.output_calibrator is not None:
+            result = self.output_calibrator(result)
+
+        return result
+
+    @torch.no_grad()
+    def assert_constraints(self) -> Dict[str, List[str]]:
+        """Asserts all layers within model satisfied specified constraints.
+
+        Asserts monotonicity pairs and output bounds for categorical calibrators,
+        monotonicity and output bounds for numerical calibrators, and monotonicity and
+        weights summing to 1 if weighted_average for linear layer.
+
+        Returns:
+            A dict where key is feature_name for calibrators and 'linear' for the linear
+            layer, and value is the error messages for each layer. Layers with no error
+            messages are not present in the dictionary.
+        """
+        messages = {}
+
+        for name, calibrator in self.calibrators.items():
+            calibrator_messages = calibrator.assert_constraints()
+            if calibrator_messages:
+                messages[f"{name}_calibrator"] = calibrator_messages
+        lattice_messages = self.lattice.assert_constraints()
+        if lattice_messages:
+            messages["lattice"] = lattice_messages
+        if self.output_calibrator:
+            output_calibrator_messages = self.output_calibrator.assert_constraints()
+            if output_calibrator_messages:
+                messages["output_calibrator"] = output_calibrator_messages
+
+        return messages
+
+    @torch.no_grad()
+    def constrain(self) -> None:
+        """Constrains the model into desired constraints specified by the config."""
+        for calibrator in self.calibrators.values():
+            calibrator.constrain()
+        self.lattice.constrain()
+        if self.output_calibrator:
+            self.output_calibrator.constrain()
