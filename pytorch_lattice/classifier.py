7d5ce66778dd384733c7841d74412eac6793edfb
William Bakst <will@sotai.ai>
2023-11-10 14:53:21 -0800

    chore: add function template for  in classifier class.

--- pytorch_lattice/classifier.py
+++ pytorch_lattice/classifier.py
@@ -1,4 +1,9 @@
 """A class for training classifiers on tabular data using calibrated models."""
+from __future__ import annotations
+
+import numpy as np
+import pandas as pd
+
 from .configs import FeatureConfig
 
 
@@ -10,7 +15,9 @@ class Classifier:
     Example:
     ```python
     X, y = pyl.datasets.heart()
-    clf = pyl.Classifier(X.columns).fit(X, y)
+    clf = pyl.Classifier(X.columns)
+    clf.configure("age").num_keypoints(10).monotonicity("increasing")
+    clf.fit(X, y)
     ```
 
     Attributes:
@@ -27,3 +34,7 @@ class Classifier:
     def configure(self, feature_name: str):
         """Returns a `FeatureConfig` object for the given feature name."""
         return self.feature_configs[feature_name]
+
+    def fit(self, X: pd.DataFrame, y: np.ndarray) -> Classifier:
+        """Returns this classifier fit to the given data."""
+        raise NotImplementedError()
