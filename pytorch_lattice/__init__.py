39305ca0836155f305f4a01529c1769f1ae58905
William Bakst <will@sotai.ai>
2023-11-13 10:28:46 -0800

    chore: update version number to reflect fixes

--- pytorch_lattice/__init__.py
+++ pytorch_lattice/__init__.py
@@ -3,7 +3,7 @@
 # This version must always be one version ahead of the current release, so it
 # matches the current state of development, which will always be ahead of the
 # current release. Use Semantic Versioning.
-__version__ = "0.0.0"
+__version__ = "0.0.3"
 
 from . import datasets, utils
 from .classifier import Classifier
